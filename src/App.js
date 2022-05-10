5b52337388d59d5ab516365e7c1ee6f899065397
klupsky <youremail@example.com>
2022-05-10 13:43:36 +0200

    Add version with main to dos done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import './App.css';
 import randomColor from 'randomcolor';
 import React, { useState } from 'react';
 
-function App() {
+export default function App() {
   const [color, setColor] = useState(randomColor.randomColor());
+  // const [hue, setHue] = useState(randomColor.hue());
 
   return (
-    <div className="Generator" style={{ backgroundColor: color }}>
+    <div className="Generator">
       <button onClick={() => setColor(randomColor.randomColor())}>
         make new color
       </button>
       <br />
-      Generated Color: {color}
+      <br />
+      <div style={{ backgroundColor: color, width: '300px', height: '300px' }}>
+        Generated Color: {color}
+      </div>
+      <br />
+      {/* <div>
+        <input
+          value={hue}
+          onChange={(event) => setHue(event.randomColor.hue())}
+        />
+      </div> */}
     </div>
   );
 }
-
-export default App;
